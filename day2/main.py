f65e408b8049ed89b2eae33bf86a5b1cbc4a7c29
Bobbitt, Avery <bobbit82@rowan.edu>
2023-12-04 15:37:17 -0500

    day 2 part 1 done

--- day2/main.py
+++ day2/main.py
@@ -13,27 +13,30 @@ def is_possible(game):
     for red_subset in red_subsets:
         if red_subset:
             reds.append(int(red_subset[0].split()[0]))
+    print(f"REDS: {reds}")
 
     for r in reds:
         if r > 12:
-            return False
-
-    blues = []
-    for blue_subset in blue_subsets:
-        if blue_subset:
-            blues.append(int(blue_subset[0].split()[0]))
-
-    for b in blues:
-        if b > 13:
             return False
 
     greens = []
     for green_subset in green_subsets:
         if green_subset:
             greens.append(int(green_subset[0].split()[0]))
+    print(f"GREENS: {greens}")
 
     for g in greens:
-        if g > 14:
+        if g > 13:
+            return False
+
+    blues = []
+    for blue_subset in blue_subsets:
+        if blue_subset:
+            blues.append(int(blue_subset[0].split()[0]))
+    print(f"BLUES: {blues}")
+
+    for b in blues:
+        if b > 14:
             return False
 
     return True
@@ -46,10 +49,16 @@ def main():
         current_game = 0
 
         for game in f:
+            print("*********************************")
+            print(f"TESTING GAME: {game}")
             current_game += 1
-            
+
             if is_possible(game):
+                print("GAME IS POSSIBLE")
                 result += current_game
+                print(f"CURRENT TOTAL: {result}")
+            else:
+                print("GAME IS NOT POSSIBLE")
 
     print(result)
 
